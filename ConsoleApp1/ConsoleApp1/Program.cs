f2ffaf5688574e38449a48f6fa192db55043fbe9
KW28042 <s28042@pjwstk.edu.pl>
2024-03-12 19:13:29 +0100

    Implement Average() function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Modification 3");
+
+static double Average(int[] integers)
+{
+    int sum = 0;
+    foreach (var i in integers) sum += i;
+    return (double)sum / integers.Length;
+}
